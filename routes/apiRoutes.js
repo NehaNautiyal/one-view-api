6aeac3817ad4b558d9cfb87521c66aeb1da8819a
Neha Nautiyal <nehahaha@gmail.com>
2019-07-04 20:47:01 -0400

    changed link for scraping to exclude product name

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -41,7 +41,7 @@ module.exports = function (app) {
 
 
         for (let page = 1; page < (totalReviewCount / 10); page++) {
-            axios.get("https://www.amazon.com/King-Koil-Luxury-Raised-Mattress/product-reviews/B06XWG7H3S/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber=" + page)
+            axios.get("https://www.amazon.com/product-reviews/B06XWG7H3S/ref=cm_cr_arp_d_viewopt_srt?ie=UTF8&reviewerType=avp_only_reviews&pageNumber=" + page + "&sortBy=recent")
                 .then((response) => {
 
                     var $ = cheerio.load(response.data);
@@ -73,83 +73,88 @@ module.exports = function (app) {
                         // Push to an empty array
                         author.push($(this).text().trim());
                     });
-                
+
+                    // Create a new document in Mongo by creating a `result` object built from scraping
+                    // let result = {};
+                    // for ()
+                    // db.Review.create(result)
+                    //     .then(function (dbReview) {
+                    //         console.log("Created!")
+                    //     })
+                    //     .catch(function (err) {
+                    //         // If an error occurred, log it
+                    //         console.log(err);
+                    //     });
+
+
 
                 })
-            }
+        }
 
-        // Create a new Article using the `result` object built from scraping
-        // db.Review.create(result)
-        //     .then(function (dbReview) {
-        //         console.log("Created!")
-        //     })
-        //     .catch(function (err) {
-        //         // If an error occurred, log it
-        //         console.log(err);
-        //     });
+
 
 
 
         // Send a message to the client
         res.redirect("/");
-});
+    });
 
-// Route for getting all Reviews from the db
-app.get("/api/reviews", function (req, res) {
-    // grabs all of the reviews
-    db.Review.find({})
-        .then(function (articles) {
-            res.json(articles);
-        })
-        .catch(function (error) {
-            res.json(error);
-        });
-});
+    // Route for getting all Reviews from the db
+    app.get("/api/reviews", function (req, res) {
+        // grabs all of the reviews
+        db.Review.find({})
+            .then(function (articles) {
+                res.json(articles);
+            })
+            .catch(function (error) {
+                res.json(error);
+            });
+    });
 
-// Route for deleting all Reviews from the db
-app.delete("/api/reviews", function (req, res) {
-    // grabs all of the reviews
-    db.Review.deleteMany()
-        .then(function (reviews) {
-            res.json(reviews);
-        })
-        .catch(function (error) {
-            res.json(error);
-        });
-});
+    // Route for deleting all Reviews from the db
+    app.delete("/api/reviews", function (req, res) {
+        // grabs all of the reviews
+        db.Review.deleteMany()
+            .then(function (reviews) {
+                res.json(reviews);
+            })
+            .catch(function (error) {
+                res.json(error);
+            });
+    });
 
-// Route to get only saved reviews
-app.get("/api/reviews/saved", function (req, res) {
-    db.Review.find({ saved: true })
-        .then(function (reviews) {
-            res.json(reviews);
-        })
-        .catch(function (error) {
-            res.json(error);
-        });
-});
+    // Route to get only saved reviews
+    app.get("/api/reviews/saved", function (req, res) {
+        db.Review.find({ saved: true })
+            .then(function (reviews) {
+                res.json(reviews);
+            })
+            .catch(function (error) {
+                res.json(error);
+            });
+    });
 
 
-app.put("/reviews/saved/:id", function (req, res) {
-    // grabs all of the reviews
-    db.Review.findOneAndUpdate({ _id: req.params.id }, { saved: false }, { new: true })
-        .then(function (reviews) {
-            res.json(reviews);
-        })
-        .catch(function (error) {
-            res.json(error);
-        });
-});
+    app.put("/reviews/saved/:id", function (req, res) {
+        // grabs all of the reviews
+        db.Review.findOneAndUpdate({ _id: req.params.id }, { saved: false }, { new: true })
+            .then(function (reviews) {
+                res.json(reviews);
+            })
+            .catch(function (error) {
+                res.json(error);
+            });
+    });
 
-app.get("/reviews/saved/:id", function (req, res) {
-    // grabs all of the reviews
-    db.Review.findOne({ _id: req.params.id })
-        .then(function (reviews) {
-            res.json(reviews);
-        })
-        .catch(function (error) {
-            res.json(error);
-        });
-});
+    app.get("/reviews/saved/:id", function (req, res) {
+        // grabs all of the reviews
+        db.Review.findOne({ _id: req.params.id })
+            .then(function (reviews) {
+                res.json(reviews);
+            })
+            .catch(function (error) {
+                res.json(error);
+            });
+    });
 
 };
