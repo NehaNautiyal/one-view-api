17452fe1fa13325fee9fe62d1b22425c03bf005e
Neha Nautiyal <nehahaha@gmail.com>
2019-07-06 21:55:14 -0400

    added urlencoder

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/oneviewreviews";
 
