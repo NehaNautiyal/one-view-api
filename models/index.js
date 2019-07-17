b1a90cccdc0e59c7dd951202adf5deaf5363bdf1
wabbot <wabbot24@gmail.com>
2019-07-17 00:07:41 -0400

    savereviews path works now

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   Review: require('./review'),
   Analysis: require('./analysis'),
-  User: require('./user')
+  User: require('./user'),
+  SaveReview: require('./savereview')
 }
